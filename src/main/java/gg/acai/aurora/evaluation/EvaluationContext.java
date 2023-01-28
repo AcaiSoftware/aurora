009118adb9f8dc4405a5c0eda93377312261f450
Clouke <76826179+Clouke@users.noreply.github.com>
2023-01-28 19:21:30 +0100

    `Performer` --> `Evaluation`

--- src/main/java/gg/acai/aurora/evaluation/EvaluationContext.java
+++ src/main/java/gg/acai/aurora/evaluation/EvaluationContext.java
@@ -1,11 +1,11 @@
-package gg.acai.aurora.performer;
+package gg.acai.aurora.evaluation;
 
 /**
  * @author Clouke
  * @since 24.01.2023 12:57
  * © Acai - All Rights Reserved
  */
-public enum PerformerContext {
+public enum EvaluationContext {
     LINEAR_REGRESSION,
     EUCLIDEAN_DISTANCE
 }
