02d3ccd6324615f92c56e33c1f3f9846381de3bc
Clouke <76826179+Clouke@users.noreply.github.com>
2023-01-28 19:21:27 +0100

    `Performer` --> `Evaluation`

---
+++ src/main/java/gg/acai/aurora/evaluation/Evaluator.java
@@ -0,0 +1,12 @@
+package gg.acai.aurora.evaluation;
+
+/**
+ * @author Clouke
+ * @since 24.01.2023 12:56
+ * © Acai - All Rights Reserved
+ */
+public interface Evaluator {
+
+    EvaluationContext getContext();
+
+}
