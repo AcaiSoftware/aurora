bf56f2ee555f0badfb2140af6d67a76239060f13
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-27 20:03:59 +0100

    renamed from `EvaluationContext`

--- src/main/java/gg/acai/aurora/core/ml/MLContext.java
+++ src/main/java/gg/acai/aurora/core/ml/MLContext.java
@@ -1,11 +1,11 @@
-package gg.acai.aurora.evaluation;
+package gg.acai.aurora.core.ml;
 
 /**
  * @author Clouke
  * @since 24.01.2023 12:57
  * © Acai - All Rights Reserved
  */
-public enum EvaluationContext {
+public enum MLContext {
     LINEAR_REGRESSION,
     NEURAL_NETWORK
 }
