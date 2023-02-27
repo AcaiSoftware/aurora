5be0c166cd6bce392583e6c3ec6abca45daf743c
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-27 20:03:46 +0100

    `Evaluator` --> `ML`

---
+++ src/main/java/gg/acai/aurora/core/ml/ML.java
@@ -0,0 +1,12 @@
+package gg.acai.aurora.core.ml;
+
+/**
+ * @author Clouke
+ * @since 24.01.2023 12:56
+ * © Acai - All Rights Reserved
+ */
+public interface ML {
+
+    MLContext getContext();
+
+}
