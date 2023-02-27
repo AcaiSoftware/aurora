a592ba3ee7bdee971d5392937894b45ca62c748a
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-27 19:00:43 +0100

    create `ModelSerializer` marker, holds `String#serialize()`, future support for `deserialization`?

---
+++ src/main/java/gg/acai/aurora/model/ModelSerializer.java
@@ -0,0 +1,12 @@
+package gg.acai.aurora.model;
+
+/**
+ * @author Clouke
+ * @since 29.01.2023 03:55
+ * © Acava - All Rights Reserved
+ */
+public interface ModelSerializer {
+
+    String serialize();
+
+}
