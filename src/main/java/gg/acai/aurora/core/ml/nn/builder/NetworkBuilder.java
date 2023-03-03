969a653247e04432cc5968d6e1953d832ed6f2b9
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-03 19:20:14 +0100

    create `NetworkBuilder`, delegates to `model` & `training`

---
+++ src/main/java/gg/acai/aurora/core/ml/nn/builder/NetworkBuilder.java
@@ -0,0 +1,19 @@
+package gg.acai.aurora.core.ml.nn.builder;
+
+import gg.acai.aurora.core.ml.nn.TrainingBuilder;
+
+/**
+ * @author Clouke
+ * @since 02.03.2023 13:09
+ * © Aurora - All Rights Reserved
+ */
+public class NetworkBuilder extends AbstractNetworkBuilder {
+
+    public static TrainingBuilder training() {
+        return new TrainingBuilder();
+    }
+
+    public static ModelBuilder model() {
+        return new ModelBuilder();
+    }
+}
