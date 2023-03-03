eb6b1cc34b448726a88ffc755eac96eab5ef7d46
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-03 19:21:11 +0100

    implementation of `TimeEstimator`, for estimating average time left of a training session

---
+++ src/main/java/gg/acai/aurora/core/ml/TrainingTimeEstimator.java
@@ -0,0 +1,38 @@
+package gg.acai.aurora.core.ml;
+
+import gg.acai.aurora.core.TimeEstimator;
+
+/**
+ * @author Clouke
+ * @author Jonathan
+ * @since 09.02.2023 17:10
+ * © Acava - All Rights Reserved
+ */
+public final class TrainingTimeEstimator implements TimeEstimator<Integer> {
+
+    private long last = -1;
+    private final int maxEpochs;
+    private double average = -1.0;
+
+    public TrainingTimeEstimator(int maxEpochs) {
+        this.maxEpochs = maxEpochs;
+    }
+
+    @Override
+    public void tick() {
+        last = System.nanoTime();
+    }
+
+    @Override
+    public double estimated(Integer currentEpoch) {
+        if (last == -1) return 0.0;
+
+        long now = System.nanoTime();
+        long diff = now - last;
+
+        double latest = (diff * (maxEpochs - currentEpoch)) / 1e9;
+        average = average == -1.0 ? latest : average * (5.0 / 6.0) + latest * (1.0 / 6.0);
+
+        return Math.round(average * 10.0) / 10.0;
+    }
+}
