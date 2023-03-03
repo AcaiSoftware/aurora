6522643226d587a402e1cc79c5724bcd2fc78193
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-03 19:16:48 +0100

    `@Compat` annotation for marking compatible versions

---
+++ src/main/java/gg/acai/aurora/Compat.java
@@ -0,0 +1,14 @@
+package gg.acai.aurora;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author Clouke
+ * @since 03.03.2023 13:33
+ * © Aurora - All Rights Reserved
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Compat {
+  String[] value();
+}
