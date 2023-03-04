9c217c6bf40e87c7fe2bc66a561a784dd6d1a95d
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-04 14:22:42 +0100

    moved from `builder`

--- src/main/java/gg/acai/aurora/ml/nn/ModelBuilder.java
+++ src/main/java/gg/acai/aurora/ml/nn/ModelBuilder.java
@@ -1,4 +1,4 @@
-package gg.acai.aurora.ml.nn.builder;
+package gg.acai.aurora.ml.nn;
 
 import gg.acai.acava.Requisites;
 import gg.acai.acava.collect.Mutability;
@@ -6,8 +6,6 @@ import gg.acai.acava.commons.graph.Graph;
 import gg.acai.aurora.Aurora;
 import gg.acai.aurora.Compatibility;
 import gg.acai.aurora.GsonSpec;
-import gg.acai.aurora.ml.nn.AbstractNeuralNetwork;
-import gg.acai.aurora.ml.nn.NeuralNetworkModel;
 import gg.acai.aurora.exception.IncompatibleModelException;
 import gg.acai.aurora.exception.InvalidModelException;
 
