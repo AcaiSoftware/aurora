9f424e5c9d13f8cac3d8e3f8307724e078a90eeb
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-04 14:24:57 +0100

    delete `AbstractNetworkBuilder`, optimize imports

--- src/main/java/gg/acai/aurora/ml/nn/NetworkBuilder.java
+++ src/main/java/gg/acai/aurora/ml/nn/NetworkBuilder.java
@@ -1,15 +1,11 @@
 package gg.acai.aurora.ml.nn;
 
-import gg.acai.aurora.ml.nn.AbstractNetworkBuilder;
-import gg.acai.aurora.ml.nn.ModelBuilder;
-import gg.acai.aurora.ml.nn.TrainingBuilder;
-
 /**
  * @author Clouke
  * @since 02.03.2023 13:09
  * © Aurora - All Rights Reserved
  */
-public class NetworkBuilder extends AbstractNetworkBuilder {
+public final class NetworkBuilder {
 
     public static TrainingBuilder training() {
         return new TrainingBuilder();
