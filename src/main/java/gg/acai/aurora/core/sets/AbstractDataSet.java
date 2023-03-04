554656d8f63b23be68fb59e48ba62d4582e1dd59
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-04 14:17:33 +0100

    minor tweaks, indent changes

--- src/main/java/gg/acai/aurora/core/sets/AbstractDataSet.java
+++ src/main/java/gg/acai/aurora/core/sets/AbstractDataSet.java
@@ -1,6 +1,6 @@
 package gg.acai.aurora.core.sets;
 
-import com.google.gson.Gson;
+import gg.acai.aurora.GsonSpec;
 
 /**
  * @author Clouke
@@ -9,73 +9,82 @@ import com.google.gson.Gson;
  */
 public abstract class AbstractDataSet implements DataSet {
 
-    protected static final Gson GSON = new Gson();
-    private static final int INITIAL_SIZE = 16;
+  private static final int INITIAL_SIZE = 16;
+  private static final Object LOCK = new Object();
 
-    protected double[][] inputs;
-    protected double[][] targets;
+  protected double[][] inputs;
+  protected double[][] targets;
 
-    public AbstractDataSet(int baseSize) {
-        this.inputs = new double[baseSize][];
-        this.targets = new double[baseSize][];
+  public AbstractDataSet(int baseSize) {
+    this.inputs = new double[baseSize][];
+    this.targets = new double[baseSize][];
+  }
+
+  public AbstractDataSet() {
+    this(INITIAL_SIZE);
+  }
+
+  @Override
+  public double[][] inputs() {
+    return inputs;
+  }
+
+  @Override
+  public double[][] targets() {
+    return targets;
+  }
+
+  @Override
+  public void add(double[][] input, double[][] target) {
+    if (input.length != target.length)
+      throw new IllegalArgumentException("Inputs and targets must be the same size");
+
+    synchronized (LOCK) {
+      int size = inputs.length;
+      int newSize = size + input.length;
+
+      if (newSize > size) {
+        double[][] newInputs = new double[newSize][];
+        double[][] newTargets = new double[newSize][];
+
+        System.arraycopy(inputs, 0, newInputs, 0, size);
+        System.arraycopy(targets, 0, newTargets, 0, size);
+
+        inputs = newInputs;
+        targets = newTargets;
+      }
+
+      System.arraycopy(input, 0, inputs, size, input.length);
+      System.arraycopy(target, 0, targets, size, target.length);
     }
+  }
 
-    public AbstractDataSet() {
-        this(INITIAL_SIZE);
+  @Override
+  public void add(double[][] input, boolean[][] target) {
+    double[][] converted = new double[target.length][target[0].length];
+    for (int i = 0; i < target.length; i++) {
+      for (int j = 0; j < target[i].length; j++) {
+        converted[i][j] = target[i][j] ? 1 : 0;
+      }
     }
+    add(input, converted);
+  }
 
-    @Override
-    public double[][] inputs() {
-        return inputs;
+  @Override
+  public final void close() {
+    synchronized (this) {
+      inputs = null;
+      targets = null;
     }
+  }
 
-    @Override
-    public double[][] targets() {
-        return targets;
-    }
+  @Override
+  public String serialize() {
+    return GsonSpec.standard().toJson(this);
+  }
 
-    @Override @SuppressWarnings("all")
-    public void add(double[][] input, double[][] target) {
-        if (input.length != target.length)
-            throw new IllegalArgumentException("Inputs and targets must be the same size");
-
-        synchronized (inputs) {
-            synchronized (targets) {
-                int size = inputs.length;
-                int newSize = size + input.length;
-
-                if (newSize > size) {
-                    double[][] newInputs = new double[newSize][];
-                    double[][] newTargets = new double[newSize][];
-
-                    System.arraycopy(inputs, 0, newInputs, 0, size);
-                    System.arraycopy(targets, 0, newTargets, 0, size);
-
-                    inputs = newInputs;
-                    targets = newTargets;
-                }
-
-                System.arraycopy(input, 0, inputs, size, input.length);
-                System.arraycopy(target, 0, targets, size, target.length);
-            }
-        }
-    }
-
-    @Override
-    public final void close() {
-        synchronized (this) {
-            inputs = null;
-            targets = null;
-        }
-    }
-
-    @Override
-    public String serialize() {
-        return GSON.toJson(this);
-    }
-
-    @Override
-    public int size() {
-        return inputs.length;
-    }
+  @Override
+  public int size() {
+    return inputs.length;
+  }
 }
