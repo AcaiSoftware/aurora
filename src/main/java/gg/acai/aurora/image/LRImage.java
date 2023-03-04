709915e026d9ef6d9dfa32321ead0eb324e474a6
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-04 14:20:34 +0100

    moved from `core`

--- src/main/java/gg/acai/aurora/image/LRImage.java
+++ src/main/java/gg/acai/aurora/image/LRImage.java
@@ -1,7 +1,7 @@
 package gg.acai.aurora.image;
 
 import com.mitchtalmadge.asciigraph.ASCIIGraph;
-import gg.acai.aurora.core.ml.lr.LRResult;
+import gg.acai.aurora.ml.lr.LRResult;
 
 /**
  * @author Clouke
