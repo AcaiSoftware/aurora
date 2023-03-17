18bc401f2525ef878adcc6e993528545e1360d8f
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-17 19:23:07 +0100

    fix indents

--- src/main/java/gg/acai/aurora/image/DrawVectorImage.java
+++ src/main/java/gg/acai/aurora/image/DrawVectorImage.java
@@ -9,54 +9,54 @@ import gg.acai.aurora.Vec2D;
  */
 public class DrawVectorImage implements Image {
 
-    private final Vec2D points;
+  private final Vec2D points;
 
-    public DrawVectorImage(Vec2D points) {
-        this.points = points;
+  public DrawVectorImage(Vec2D points) {
+    this.points = points;
+  }
+
+  @Override
+  public String draw() {
+    double[] x = points.x();
+    double[] y = points.y();
+
+    // Find the min and max values of x and y
+    double minX = Double.MAX_VALUE;
+    double maxX = Double.MIN_VALUE;
+    double minY = Double.MAX_VALUE;
+    double maxY = Double.MIN_VALUE;
+
+    for (int i = 0; i < x.length; i++) {
+      minX = Math.min(minX, x[i]);
+      maxX = Math.max(maxX, x[i]);
+      minY = Math.min(minY, y[i]);
+      maxY = Math.max(maxY, y[i]);
     }
 
-    @Override
-    public String draw() {
-        double[] x = points.x();
-        double[] y = points.y();
+    // Scale the values to fit within the console window
+    double xScale = 60 / (maxX - minX);
+    double yScale = 20 / (maxY - minY);
 
-        // Find the min and max values of x and y
-        double minX = Double.MAX_VALUE;
-        double maxX = Double.MIN_VALUE;
-        double minY = Double.MAX_VALUE;
-        double maxY = Double.MIN_VALUE;
-
-        for (int i = 0; i < x.length; i++) {
-            minX = Math.min(minX, x[i]);
-            maxX = Math.max(maxX, x[i]);
-            minY = Math.min(minY, y[i]);
-            maxY = Math.max(maxY, y[i]);
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < yScale * (maxY - minY); i++) {
+      for (int j = 0; j < xScale * (maxX - minX); j++) {
+        boolean pointFound = false;
+        for (int k = 0; k < x.length; k++) {
+          if (Math.round(x[k] * xScale) == j && Math.round(y[k] * yScale) == i) {
+            builder.append("*");
+            pointFound = true;
+            break;
+          }
         }
-
-        // Scale the values to fit within the console window
-        double xScale = 60 / (maxX - minX);
-        double yScale = 20 / (maxY - minY);
-
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < yScale * (maxY - minY); i++) {
-            for (int j = 0; j < xScale * (maxX - minX); j++) {
-                boolean pointFound = false;
-                for (int k = 0; k < x.length; k++) {
-                    if (Math.round(x[k] * xScale) == j && Math.round(y[k] * yScale) == i) {
-                        builder.append("*");
-                        pointFound = true;
-                        break;
-                    }
-                }
-                if (!pointFound) builder.append(" ");
-            }
-            builder.append("\n");
-        }
-        return builder.toString();
+        if (!pointFound) builder.append(" ");
+      }
+      builder.append("\n");
     }
+    return builder.toString();
+  }
 
-    @Override
-    public String description() {
-        return "Visualizes a 2D vector's points";
-    }
+  @Override
+  public String description() {
+    return "Visualizes a 2D vector's points";
+  }
 }
