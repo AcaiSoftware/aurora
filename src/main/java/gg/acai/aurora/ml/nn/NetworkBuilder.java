b94ceddd4998cb7b78738c940e7944ffb4bdf0c3
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-17 18:59:37 +0100

    fix indents

--- src/main/java/gg/acai/aurora/ml/nn/NetworkBuilder.java
+++ src/main/java/gg/acai/aurora/ml/nn/NetworkBuilder.java
@@ -7,11 +7,12 @@ package gg.acai.aurora.ml.nn;
  */
 public final class NetworkBuilder { // TODO: Rename to factory
 
-    public static TrainingBuilder training() {
-        return new TrainingBuilder();
-    }
+  public static TrainingBuilder training() {
+    return new TrainingBuilder();
+  }
 
-    public static ModelBuilder model() {
-        return new ModelBuilder();
-    }
+  public static ModelLoader model() {
+    return new ModelLoader();
+  }
+
 }
