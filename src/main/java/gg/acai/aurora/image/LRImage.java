bef243d024bdc839cd7ba6e87f3f6304c3eb518c
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-17 19:23:12 +0100

    fix indents

--- src/main/java/gg/acai/aurora/image/LRImage.java
+++ src/main/java/gg/acai/aurora/image/LRImage.java
@@ -10,37 +10,37 @@ import gg.acai.aurora.ml.lr.LRResult;
  */
 public class LRImage implements Image {
 
-    private final LRResult regression;
-    private int size = 21;
+  private final LRResult regression;
+  private int size = 21;
 
-    public LRImage(LRResult regression, int size) {
-        this.regression = regression;
-        this.size = size;
+  public LRImage(LRResult regression, int size) {
+    this.regression = regression;
+    this.size = size;
+  }
+
+  public LRImage(LRResult regression) {
+    this.regression = regression;
+  }
+
+  @Override
+  public String draw() {
+    double slope = regression.slope();
+    double intercept = regression.intercept();
+
+    double[] ys = new double[size];
+
+    int index = 0;
+    for (int x = -10; x <= 10; x++) {
+      double y = slope * x + intercept;
+      ys[index] = y;
+      index++;
     }
 
-    public LRImage(LRResult regression) {
-        this.regression = regression;
-    }
+    return ASCIIGraph.fromSeries(ys).plot();
+  }
 
-    @Override
-    public String draw() {
-        double slope = regression.slope();
-        double intercept = regression.intercept();
-
-        double[] ys = new double[size];
-
-        int index = 0;
-        for (int x = -10; x <= 10; x++) {
-            double y = slope * x + intercept;
-            ys[index] = y;
-            index++;
-        }
-
-        return ASCIIGraph.fromSeries(ys).plot();
-    }
-
-    @Override
-    public String description() {
-        return "Draws a linear regression graph of the data.";
-    }
+  @Override
+  public String description() {
+    return "Draws a linear regression graph of the data.";
+  }
 }
