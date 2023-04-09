5b31cde1b537d109b60007b76d9a46ad7875843b
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-09 20:38:12 +0200

    create `Evaluator`

---
+++ src/main/java/gg/acai/aurora/Evaluator.java
@@ -0,0 +1,11 @@
+package gg.acai.aurora;
+
+/**
+ * @author Clouke
+ * @since 09.04.2023 20:28
+ * © Aurora - All Rights Reserved
+ */
+@FunctionalInterface
+public interface Evaluator {
+  double evaluate();
+}
