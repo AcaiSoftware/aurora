d519f9e674ae6f0a521ae3862c62d732717f7dfb
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-13 01:00:24 +0200

    add `trySerialize(...)`

--- core/src/main/java/gg/acai/aurora/Serializer.java
+++ core/src/main/java/gg/acai/aurora/Serializer.java
@@ -1,5 +1,7 @@
 package gg.acai.aurora;
 
+import javax.annotation.Nonnull;
+
 /**
  * @author Clouke
  * @since 29.01.2023 03:55
@@ -7,5 +9,15 @@ package gg.acai.aurora;
  */
 @FunctionalInterface
 public interface Serializer {
+
   String serialize();
+
+  static <T> String trySerialize(@Nonnull T t) {
+    if (t instanceof Serializer) {
+      return ((Serializer) t).serialize();
+    }
+    String s = t.toString();
+    return s.startsWith(t.getClass().getName() + "@") && s.contains("0x") ? null : s;
+  }
+
 }
