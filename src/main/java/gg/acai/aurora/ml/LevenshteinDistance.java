c2c84fd7679f055d43f34e3a1c713db43fda0c74
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-12 20:15:42 +0200

    easier readability

--- src/main/java/gg/acai/aurora/ml/LevenshteinDistance.java
+++ src/main/java/gg/acai/aurora/ml/LevenshteinDistance.java
@@ -8,18 +8,21 @@ package gg.acai.aurora.ml;
 public class LevenshteinDistance {
 
   public static int compute(CharSequence lhs, CharSequence rhs) {
-    int[][] distance = new int[lhs.length() + 1][rhs.length() + 1];
+    int l_length = lhs.length();
+    int r_length = rhs.length();
+    int[][] distance = new int[l_length + 1][r_length + 1];
 
-    for (int i = 0; i <= lhs.length(); i++)
-      distance[i][0] = i;
-    for (int j = 1; j <= rhs.length(); j++)
-      distance[0][j] = j;
+    for (int i = 0; i <= l_length; i++) distance[i][0] = i;
+    for (int j = 1; j <= r_length; j++) distance[0][j] = j;
 
-    for (int i = 1; i <= lhs.length(); i++)
-      for (int j = 1; j <= rhs.length(); j++)
-        distance[i][j] = Math.min(Math.min(distance[i - 1][j] + 1, distance[i][j - 1] + 1), distance[i - 1][j - 1] + ((lhs.charAt(i - 1) == rhs.charAt(j - 1)) ? 0 : 1));
+    for (int i = 1; i <= l_length; i++)
+      for (int j = 1; j <= r_length; j++)
+        distance[i][j] = Math.min(
+          Math.min(distance[i - 1][j] + 1, distance[i][j - 1] + 1),
+          distance[i - 1][j - 1] + ((lhs.charAt(i - 1) == rhs.charAt(j - 1)) ? 0 : 1)
+        );
 
-    return distance[lhs.length()][rhs.length()];
+    return distance[l_length][r_length];
   }
 
 }
