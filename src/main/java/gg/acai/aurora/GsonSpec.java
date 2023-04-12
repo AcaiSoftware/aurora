846aed8fdddd4e36bcf864bfc44aff94750b625f
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-12 20:16:27 +0200

    remove `CHATBOT` types & tests with NLP

--- src/main/java/gg/acai/aurora/GsonSpec.java
+++ src/main/java/gg/acai/aurora/GsonSpec.java
@@ -2,11 +2,8 @@ package gg.acai.aurora;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 import gg.acai.aurora.ml.ActivationFunction;
 import gg.acai.aurora.ml.ActivationFunctionSerializer;
-import gg.acai.aurora.ml.lang.ChatbotSerializer;
-import gg.acai.aurora.ml.lang.ChatbotV2;
 
 /**
  * @author Clouke
@@ -15,17 +12,13 @@ import gg.acai.aurora.ml.lang.ChatbotV2;
  */
 public final class GsonSpec {
 
-  private static final TypeToken<ChatbotV2> CHATBOT_TYPE = new TypeToken<ChatbotV2>() {};
-
   private static final Gson STANDARD = new GsonBuilder()
     .registerTypeAdapter(ActivationFunction.class, new ActivationFunctionSerializer())
-    .registerTypeAdapter(CHATBOT_TYPE.getType(), new ChatbotSerializer())
     .create();
 
   private static final Gson PRETTY = new GsonBuilder()
     .setPrettyPrinting()
     .registerTypeAdapter(ActivationFunction.class, new ActivationFunctionSerializer())
-    .registerTypeAdapter(CHATBOT_TYPE.getType(), new ChatbotSerializer())
     .create();
 
   public static Gson standard() {
@@ -39,9 +32,4 @@ public final class GsonSpec {
   public static Gson get(boolean pretty) {
     return pretty ? pretty() : standard();
   }
-
-  public static TypeToken<ChatbotV2> chatbotType() {
-    return CHATBOT_TYPE;
-  }
-
 }
