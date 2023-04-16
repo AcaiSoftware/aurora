6742e8e46da03e353b073b69d42cf558ef4dd59d
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-16 16:00:34 +0200

    create `ModelLoaderOptionsBuilder`

---
+++ core/src/main/java/gg/acai/aurora/ModelLoaderOptionsBuilder.java
@@ -0,0 +1,38 @@
+package gg.acai.aurora;
+
+/**
+ * @author Clouke
+ * @since 16.04.2023 15:44
+ * © Aurora - All Rights Reserved
+ */
+public class ModelLoaderOptionsBuilder {
+
+  private String name;
+  private String saveDirectory;
+  private boolean saveOnClose = true;
+  private boolean ignoreVersion = false;
+
+  public ModelLoaderOptionsBuilder name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public ModelLoaderOptionsBuilder saveDirectory(String saveDirectory) {
+    this.saveDirectory = saveDirectory;
+    return this;
+  }
+
+  public ModelLoaderOptionsBuilder enableAutoSave() {
+    this.saveOnClose = false;
+    return this;
+  }
+
+  public ModelLoaderOptionsBuilder ignoreVersion() {
+    this.ignoreVersion = true;
+    return this;
+  }
+
+  public ModelLoaderOptions build() {
+    return new ModelLoaderOptions(name, saveDirectory, saveOnClose, ignoreVersion);
+  }
+}
