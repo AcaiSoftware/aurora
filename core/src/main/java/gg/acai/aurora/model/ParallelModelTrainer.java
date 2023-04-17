753297e7f19a62c4bd9284a0152f1fc761c40f62
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-17 03:21:29 +0200

    create `ParallelModelTraining`, allowing multithreaded training

---
+++ core/src/main/java/gg/acai/aurora/model/ParallelModelTrainer.java
@@ -0,0 +1,24 @@
+package gg.acai.aurora.model;
+
+import gg.acai.acava.collect.maps.BiMap;
+import gg.acai.aurora.ml.Trainable;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * @author Clouke
+ * @since 17.04.2023 00:59
+ * © Aurora - All Rights Reserved
+ */
+public interface ParallelModelTrainer {
+
+  void begin(Consumer<ParallelModelTrainer> completion);
+
+  ParallelModelTrainer add(Trainable trainer, double[][] inputs, double[][] outputs);
+
+  List<Thread> workers();
+
+  BiMap<Trainable, double[][], double[][]> asMap();
+
+}
