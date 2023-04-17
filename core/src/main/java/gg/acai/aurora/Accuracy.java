ba22e69337ff283b1fe5db2a75d5e368734d6af5
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-17 03:19:10 +0200

    create `Accuracy`

---
+++ core/src/main/java/gg/acai/aurora/Accuracy.java
@@ -0,0 +1,11 @@
+package gg.acai.aurora;
+
+/**
+ * @author Clouke
+ * @since 16.04.2023 22:01
+ * © Aurora - All Rights Reserved
+ */
+@FunctionalInterface
+public interface Accuracy {
+  double accuracy();
+}
