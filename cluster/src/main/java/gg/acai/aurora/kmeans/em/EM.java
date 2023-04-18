61100d3a14ddb3d796ad945abd68b05d8672836b
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-18 04:16:53 +0200

    create `EM` clusterer

---
+++ cluster/src/main/java/gg/acai/aurora/kmeans/em/EM.java
@@ -0,0 +1,20 @@
+package gg.acai.aurora.kmeans.em;
+
+import gg.acai.aurora.Clusterer;
+
+import java.util.List;
+
+/**
+ * @author Clouke
+ * @since 18.04.2023 04:07
+ * © Aurora - All Rights Reserved
+ */
+@FunctionalInterface
+public interface EM extends Clusterer {
+  /**
+   * Gets the cluster of the data points in this EM model.
+   *
+   * @return Returns cluster of the data points in this EM model.
+   */
+  List<Integer> cluster();
+}
