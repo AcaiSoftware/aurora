4dda6e43bf40bf2c4e8bda2b6b2a08a96e23c385
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-23 23:50:17 +0200

    create `Trainable` implementation of `Benchmark`

---
+++ core/src/main/java/gg/acai/aurora/benchmarks/TrainableBenchmark.java
@@ -0,0 +1,41 @@
+package gg.acai.aurora.benchmarks;
+
+import gg.acai.aurora.ml.Trainable;
+
+/**
+ * @author Clouke
+ * @since 18.04.2023 00:03
+ * © Aurora - All Rights Reserved
+ */
+public class TrainableBenchmark extends AbstractTrainableBenchmark {
+
+  public TrainableBenchmark(Trainable trainable, int warmupCycles, int benchmarkCycles, double[][] inputs, double[][] outputs) {
+    super(trainable, warmupCycles, benchmarkCycles, inputs, outputs);
+  }
+
+  @Override
+  public BenchmarkResult compose() {
+    for (int i = 0; i < warmupCycles; i++) {
+      trainable.train(inputs, outputs);
+    }
+
+    long totalTime = 0L;
+    double totalAccuracy = 0.0;
+    for (int i = 0; i < benchmarkCycles; i++) {
+      long start = System.nanoTime();
+      trainable.train(inputs, outputs);
+      long end = System.nanoTime();
+      totalTime += end - start;
+      totalAccuracy += trainable.accuracy();
+    }
+
+    int in = inputs.length;
+    double averageTimePerEpoch = (double) totalTime / benchmarkCycles;
+    double averageAccuracy = totalAccuracy / benchmarkCycles;
+    double timePerSample = averageTimePerEpoch / in;
+    double throughput = inputs.length * benchmarkCycles / averageTimePerEpoch;
+    return new WrappedBenchmarkResult(averageTimePerEpoch, averageAccuracy, timePerSample, throughput);
+  }
+
+
+}
