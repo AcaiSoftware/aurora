df589ade595798197a63dc85067cc5685cbff7b6
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-23 23:45:53 +0200

    create `Benchmark`

---
+++ core/src/main/java/gg/acai/aurora/benchmarks/Benchmark.java
@@ -0,0 +1,11 @@
+package gg.acai.aurora.benchmarks;
+
+/**
+ * @author Clouke
+ * @since 18.04.2023 00:04
+ * © Aurora - All Rights Reserved
+ */
+@FunctionalInterface
+public interface Benchmark {
+  BenchmarkResult compose();
+}
