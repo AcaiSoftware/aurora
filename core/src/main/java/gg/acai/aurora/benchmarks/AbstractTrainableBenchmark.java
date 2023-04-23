bebd3c019ed69af72c881927d68417c5a0c2cea6
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-23 23:45:37 +0200

    create `AbstractTrainableBenchmark`

---
+++ core/src/main/java/gg/acai/aurora/benchmarks/AbstractTrainableBenchmark.java
@@ -0,0 +1,26 @@
+package gg.acai.aurora.benchmarks;
+
+import gg.acai.aurora.ml.Trainable;
+
+/**
+ * @author Clouke
+ * @since 18.04.2023 03:32
+ * © Aurora - All Rights Reserved
+ */
+public abstract class AbstractTrainableBenchmark implements Benchmark {
+
+  protected final Trainable trainable;
+  protected final int warmupCycles;
+  protected final int benchmarkCycles;
+  protected final double[][] inputs;
+  protected final double[][] outputs;
+
+  public AbstractTrainableBenchmark(Trainable trainable, int warmupCycles, int benchmarkCycles, double[][] inputs, double[][] outputs) {
+    this.trainable = trainable;
+    this.warmupCycles = warmupCycles;
+    this.benchmarkCycles = benchmarkCycles;
+    this.inputs = inputs;
+    this.outputs = outputs;
+  }
+
+}
