deaa52fc7343b6bcd0d5ce7d02bc906f05816e9f
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-25 15:06:44 +0200

    remove `Immutables`

--- core/src/main/java/gg/acai/aurora/sets/DataSetBuilder.java
+++ core/src/main/java/gg/acai/aurora/sets/DataSetBuilder.java
@@ -15,8 +15,6 @@ import java.io.FileReader;
 public class DataSetBuilder {
 
   private int maxSize = -1;
-  private double[][] immutableInputs;
-  private double[][] immutableTargets;
   private DataSet imported;
 
   public DataSetBuilder maxSize(int maxSize) {
@@ -24,44 +22,18 @@ public class DataSetBuilder {
     return this;
   }
 
-  public DataSetBuilder immutable(double[][] inputs, double[][] targets) {
-    this.immutableInputs = inputs;
-    this.immutableTargets = targets;
-    return this;
-  }
-
-  public DataSetBuilder importFrom(File file) {
-    Requisites.requireNonNull(file, "file");
-    FileReader reader;
-    try {
-      reader = new FileReader(file);
-      imported = GsonSpec.standard().fromJson(reader, ImmutableDataSet.class);
-    } catch (FileNotFoundException e) {
-      throw new IllegalArgumentException("File not found: " + file.getAbsolutePath());
-    }
-    return this;
-  }
-
-  public DataSetBuilder importFrom(String json) {
-    Requisites.requireNonNull(json, "json");
-    imported = GsonSpec.standard().fromJson(json, ImmutableDataSet.class);
-    return this;
-  }
-
   public DataSet build() {
     int r = buildProcedureResult();
     switch (r) {
       case 1: return imported;
-      case 2: return new ImmutableDataSet(immutableInputs, immutableTargets);
-      case 3: return new FixedSizeDataSet(maxSize);
+      case 2: return new FixedSizeDataSet(maxSize);
       default: return new LocalDataSet();
     }
   }
 
   private int buildProcedureResult() {
     if (imported != null) return 1;
-    if (immutableInputs != null && immutableTargets != null) return 2;
-    if (maxSize != -1) return 3;
+    if (maxSize != -1) return 2;
     return 0;
   }
 
