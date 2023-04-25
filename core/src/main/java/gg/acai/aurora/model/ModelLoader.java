5cd53ad0decd1ef2323653b3242bb87b6a90ff4c
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-25 15:10:18 +0200

    more flexibility & efficiency in loading, added `GC` handling

--- core/src/main/java/gg/acai/aurora/model/ModelLoader.java
+++ core/src/main/java/gg/acai/aurora/model/ModelLoader.java
@@ -1,20 +1,15 @@
 package gg.acai.aurora.model;
 
 import gg.acai.acava.Requisites;
-import gg.acai.aurora.Aurora;
 import gg.acai.aurora.GsonSpec;
-import gg.acai.aurora.universal.Compatibility;
-import gg.acai.aurora.exception.IncompatibleModelException;
 import gg.acai.aurora.exception.InvalidModelException;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Map;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 /**
  * A flexible model loader for machine learning models.
@@ -23,124 +18,72 @@ import java.util.function.Supplier;
  * @since 02.03.2023 13:13
  * © Aurora - All Rights Reserved
  */
-public class ModelLoader {
+public class ModelLoader implements AutoCloseable {
 
-  private Model model;
-  private final Class<? extends Model> modelClass;
-  private ModelLoaderOptions options;
-  private boolean imported;
+  private String json;
 
-  public ModelLoader(Class<? extends Model> modelClass) {
-    this.modelClass = modelClass;
-  }
-
-  public ModelLoader(Supplier<Class<? extends Model>> modelClass) {
-    this(modelClass.get());
-  }
-
-  public ModelLoader importWithJson(String json) {
-    ensureImport();
-    try {
-      this.model = GsonSpec.standard().fromJson(json, modelClass);
-      mark();
-    } catch (Exception e) {
-      throw new InvalidModelException("Could not import model with json", e);
-    }
-    return this;
-  }
-
-  public ModelLoader retrieveFromHttp(String url) {
-    return retrieveFromHttp(url, null);
-  }
-
-  public ModelLoader retrieveFromHttp(String url, Map<String, String> headers) {
-    ensureImport();
-    HttpURLConnection connection = null;
-    try {
-      connection = (HttpURLConnection) new URL(url).openConnection();
-      connection.setRequestMethod("GET");
-      connection.setRequestProperty("Content-Type", "application/json");
-      if (headers != null) {
-        headers.forEach(connection::setRequestProperty);
-      }
-      connection.setDoOutput(true);
-      connection.setDoInput(true);
-      connection.connect();
-      this.model = GsonSpec.standard().fromJson(new InputStreamReader(connection.getInputStream()), modelClass);
-      mark();
-    } catch (Exception e) {
-      throw new InvalidModelException("Could not retrieve model from url " + url + "!", e);
-    } finally {
-      if (connection != null) {
-        connection.disconnect();
-      }
-    }
-    return this;
-  }
-
-  public ModelLoader options(Consumer<ModelLoaderOptionsBuilder> options) {
-    ModelLoaderOptionsBuilder builder = new ModelLoaderOptionsBuilder();
-    options.accept(builder);
-    this.options = builder.build();
-    return this;
-  }
-
-  public ModelLoader importFromFile(String path) {
-    String fixedPath = path;
-    if (!fixedPath.endsWith(".json"))
-      fixedPath += ".json";
-    return importFromFile(new File(fixedPath));
-  }
-
-  public ModelLoader importFromFile(File file) {
-    ensureImport();
-    Requisites.checkArgument(file.exists(), "Provided file does not exist: " + file.getName());
+  public ModelLoader(File file) throws InvalidModelException {
     try (FileReader reader = new FileReader(file)) {
-      this.model = GsonSpec.standard().fromJson(reader, modelClass);
-      mark();
+      StringBuilder builder = new StringBuilder();
+      BufferedReader bufferedReader = new BufferedReader(reader);
+      String line;
+      while ((line = bufferedReader.readLine()) != null) {
+        builder.append(line);
+      }
+      this.json = builder.toString();
     } catch (Exception e) {
       throw new InvalidModelException("Could not import model from file " + file.getName(), e);
     }
-    return this;
+  }
+
+  public ModelLoader(URL url) throws InvalidModelException {
+    try (InputStreamReader reader = new InputStreamReader(url.openStream())) {
+      StringBuilder builder = new StringBuilder();
+      BufferedReader bufferedReader = new BufferedReader(reader);
+      String line;
+      while ((line = bufferedReader.readLine()) != null) {
+        builder.append(line);
+      }
+      this.json = builder.toString();
+    } catch (Exception e) {
+      throw new InvalidModelException("Could not import model from url " + url.toString(), e);
+    }
+  }
+
+  public ModelLoader(String json) {
+    Requisites.requireNonNull(json, "json cannot be null");
+    this.json = json;
   }
 
   @SuppressWarnings("unchecked")
-  public <M extends Model> M build() {
-    Requisites.checkArgument(imported, "model has not been imported!");
+  public <M extends Model> M load(Class<? extends Model> modelClass, Consumer<ModelLoaderOptionsBuilder> options) {
+    Requisites.requireNonNull(modelClass, "modelClass cannot be null");
+    Model model = GsonSpec
+      .standard()
+      .fromJson(json, modelClass);
 
-    boolean ignoreVersions = false;
+    if (model == null) {
+      throw new InvalidModelException("Could not import model with json");
+    }
+
     if (options != null) {
-      model.name(options.name())
-        .saveDirectoryPath(options.saveDirectory())
-        .saveOnClose(options.saving());
-      ignoreVersions = options.ignored();
+      ModelLoaderOptionsBuilder builder = new ModelLoaderOptionsBuilder();
+      options.accept(builder);
+      ModelLoaderOptions opts = builder.build();
+      model.name(opts.name())
+        .saveOnClose(opts.saving())
+        .saveDirectoryPath(opts.saveDirectory());
     }
 
-    String currentVersion = ignoreVersions ? null : Aurora.version();
-    if (currentVersion != null) {
-      boolean compat = Compatibility.isCompatible(model.getClass());
-      if (!compat) {
-        throw new IncompatibleModelException(
-          "Incompatible model version, Current version: " + currentVersion + ", Model version: " + model.version() +
-          ", Compatible versions: " + Compatibility.getCompatibleVersions(model.getClass()) +
-          ". \nUse ModelLoader#ignoreVersionCheck() to ignore this check."
-        );
-      }
-    }
-
-    try {
-      return (M) model;
-    } catch (Exception e) {
-      throw new InvalidModelException("Could not cast model to " + modelClass.getSimpleName(), e);
-    }
+    return (M) model;
   }
 
-  private void ensureImport() {
-    if (imported) throw new IllegalStateException("Model has already been imported");
+  public <M extends Model> M load(Class<? extends Model> modelClass) {
+    return load(modelClass, null);
   }
 
-  private void mark() {
-    imported = true;
+  @Override
+  public void close() {
+    this.json = null; // Call GC
   }
-
 }
