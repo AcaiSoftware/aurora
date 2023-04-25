8311a5dcacd6f7e4ae978b502aeaa90c4652c6f5
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-25 15:09:51 +0200

    add `Date`

--- core/src/main/java/gg/acai/aurora/model/ModelArchive.java
+++ core/src/main/java/gg/acai/aurora/model/ModelArchive.java
@@ -2,6 +2,8 @@ package gg.acai.aurora.model;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.function.Consumer;
 
 /**
@@ -12,11 +14,16 @@ import java.util.function.Consumer;
 public class ModelArchive {
 
   private static final String MODEL_ARCHIVE = "model-archive";
+  private static final String DATE_FORMAT = "yyyy-MM-dd_HH-mm";
 
   @SuppressWarnings("ResultOfMethodCallIgnored")
   public static void saveModel(Model model, Consumer<? super Exception> failure) {
     String directory = model.saveDirectoryPath();
-    String name = model.nameOpt().orElse(MODEL_ARCHIVE);
+    String name = model.nameOpt()
+      .orElseGet(() -> {
+        String date = new SimpleDateFormat(DATE_FORMAT).format(new Date());
+        return MODEL_ARCHIVE + "-" + date;
+      });
     File file = new File(directory);
     if (!file.exists()) {
       file.mkdirs();
