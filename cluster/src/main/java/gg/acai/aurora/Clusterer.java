26d3396d010f814b20ffc8480e05657f8b4e21e2
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-25 15:44:32 +0200

    extend `Predictable`

--- cluster/src/main/java/gg/acai/aurora/Clusterer.java
+++ cluster/src/main/java/gg/acai/aurora/Clusterer.java
@@ -1,8 +1,10 @@
 package gg.acai.aurora;
 
+import gg.acai.aurora.ml.Predictable;
+
 /**
  * @author Clouke
  * @since 09.04.2023 20:05
  * © Aurora - All Rights Reserved
  */
-public interface Clusterer {}
+public interface Clusterer extends Predictable {}
