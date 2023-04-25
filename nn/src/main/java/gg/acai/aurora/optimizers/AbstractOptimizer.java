9030c0954d94186d5d28b4c74e5511f36e23a968
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-25 15:13:26 +0200

    create `AbstractOptimizer` extension of `Optimizer`

---
+++ nn/src/main/java/gg/acai/aurora/optimizers/AbstractOptimizer.java
@@ -0,0 +1,23 @@
+package gg.acai.aurora.optimizers;
+
+/**
+ * @author Clouke
+ * @since 25.04.2023 11:22
+ * © Aurora - All Rights Reserved
+ */
+public abstract class AbstractOptimizer implements Optimizer {
+
+  protected double learningRate;
+
+  public AbstractOptimizer(double learningRate) {
+    this.learningRate = learningRate;
+  }
+
+  public AbstractOptimizer() {}
+
+  @Override
+  public Optimizer apply(double learningRate) {
+    this.learningRate = learningRate;
+    return this;
+  }
+}
