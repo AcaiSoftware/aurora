f9ff38afefa7670676e14e0bc484180180a4a3fd
Clouke <76826179+Clouke@users.noreply.github.com>
2023-04-27 19:33:02 +0200

    feature(MLContext): implement `MLContextProvider`

--- cluster/src/main/java/gg/acai/aurora/Clusterer.java
+++ cluster/src/main/java/gg/acai/aurora/Clusterer.java
@@ -1,10 +1,19 @@
 package gg.acai.aurora;
 
-import gg.acai.aurora.ml.Predictable;
+import gg.acai.aurora.model.MLContext;
+import gg.acai.aurora.model.MLContextProvider;
+import gg.acai.aurora.model.Predictable;
 
 /**
  * @author Clouke
  * @since 09.04.2023 20:05
  * © Aurora - All Rights Reserved
  */
-public interface Clusterer extends Predictable {}
+public interface Clusterer extends Predictable, MLContextProvider {
+
+  @Override
+  default MLContext context() {
+    return MLContext.CLUSTERING;
+  }
+
+}
