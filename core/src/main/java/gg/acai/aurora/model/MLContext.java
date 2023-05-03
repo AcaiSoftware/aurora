55383c9c87cb06363f069512f5c9080d980ba772
Clouke <76826179+Clouke@users.noreply.github.com>
2023-05-03 16:20:39 +0200

    add `toString()` and `toShort`

---
+++ core/src/main/java/gg/acai/aurora/model/MLContext.java
@@ -0,0 +1,40 @@
+package gg.acai.aurora.model;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author Clouke
+ * @since 24.01.2023 12:57
+ * © Acai - All Rights Reserved
+ */
+public enum MLContext {
+  LINEAR_REGRESSION,
+  LOGISTIC_REGRESSION,
+  NEURAL_NETWORK,
+  LEARNING_VECTOR_QUANTIZATION,
+  NATURAL_LANGUAGE_PROCESSING,
+  CLUSTERING;
+
+  public String toString() {
+    String name = name();
+    String[] words = name.contains("_") ? name.split("_") : new String[]{name};
+    for (int i = 0; i < words.length; i++) {
+      words[i] = StringUtils.capitalize(words[i].toLowerCase());
+    }
+    return StringUtils.join(words, " ");
+  }
+
+  public String toShort() {
+    switch(this) {
+      case LINEAR_REGRESSION: return "LINREG";
+      case LOGISTIC_REGRESSION: return "LOGREG";
+      case NEURAL_NETWORK: return "NN";
+      case LEARNING_VECTOR_QUANTIZATION: return "LVQ";
+      case NATURAL_LANGUAGE_PROCESSING: return "NLP";
+      case CLUSTERING: return "CLUST";
+      default:
+        throw new IllegalArgumentException("Unknown MLContext value: " + this);
+    }
+  }
+
+}
