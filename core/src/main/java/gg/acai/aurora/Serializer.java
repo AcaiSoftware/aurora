1a2c22a60a93fd984426bfedecb8261dc6f8e760
Clouke <76826179+Clouke@users.noreply.github.com>
2023-05-03 16:29:29 +0200

    add docs

--- core/src/main/java/gg/acai/aurora/Serializer.java
+++ core/src/main/java/gg/acai/aurora/Serializer.java
@@ -10,13 +10,27 @@ import javax.annotation.Nonnull;
 @FunctionalInterface
 public interface Serializer {
 
+  /**
+   * Serializes this object.
+   *
+   * @return Returns the serialized object.
+   */
   String serialize();
 
+  /**
+   * Attempts to serialize the given object.
+   *
+   * @param t The object to serialize.
+   * @param <T> The type of the object to serialize.
+   * @return Returns the serialized object, or null if it could not be serialized.
+   */
   static <T> String trySerialize(@Nonnull T t) {
     if (t instanceof Serializer) {
       return ((Serializer) t).serialize();
     }
+
     String s = t.toString();
+    // We need to check for default toString() implementation and return null if it is the case.
     return s.startsWith(t.getClass().getName() + "@") && s.contains("0x") ? null : s;
   }
 
