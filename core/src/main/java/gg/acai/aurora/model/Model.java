4f7a4a24fe0572272ab8a0cbd9ab183ee38d1c60
Clouke <76826179+Clouke@users.noreply.github.com>
2023-05-03 16:20:53 +0200

    added docs

--- core/src/main/java/gg/acai/aurora/model/Model.java
+++ core/src/main/java/gg/acai/aurora/model/Model.java
@@ -14,22 +14,65 @@ import java.util.Optional;
  */
 public interface Model extends Serializer, SavePoint, Closeable {
 
+  /**
+   * Applies the name to this model.
+   *
+   * @param name The name to apply.
+   * @return Returns this model for chaining.
+   */
   Model name(String name);
 
+  /**
+   * Applies the save directory to this model.
+   *
+   * @param saveDirectory The save directory to apply.
+   * @return Returns this model for chaining.
+   */
   Model saveDirectoryPath(String saveDirectory);
 
+  /**
+   * Applies rather this model should be saved on close.
+   *
+   * @param saveOnClose The save on close to apply.
+   * @return Returns this model for chaining.
+   */
   Model saveOnClose(boolean saveOnClose);
 
+  /**
+   * Gets the version of this model.
+   *
+   * @return Returns the version of this model.
+   */
   String version();
 
+  /**
+   * Gets the save directory of this model.
+   *
+   * @return Returns the save directory of this model.
+   */
   String saveDirectoryPath();
 
+  /**
+   * Gets the name of this model.
+   *
+   * @return Returns the name of this model.
+   */
   String name();
 
+  /**
+   * Gets the name of this model as an optional.
+   *
+   * @return Returns the name of this model as an optional.
+   */
   default Optional<String> nameOpt() {
     return Optional.ofNullable(name());
   }
 
+  /**
+   * Saves this model.
+   *
+   * @throws ModelStorageException If an error occurs while saving this model.
+   */
   @Override
   default void save() throws ModelStorageException {
     ModelArchive.saveModel(this, e -> {
@@ -37,6 +80,12 @@ public interface Model extends Serializer, SavePoint, Closeable {
     });
   }
 
+  /**
+   * Saves this model to the specified save directory.
+   *
+   * @param saveDirectory The save directory to save this model to.
+   * @throws ModelStorageException If an error occurs while saving this model.
+   */
   default void save(String saveDirectory) throws ModelStorageException {
     saveDirectoryPath(saveDirectory);
     save();
