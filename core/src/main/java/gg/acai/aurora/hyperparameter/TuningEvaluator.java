928d0f97889e1e1efed53a3da30ee78280373517
Clouke <76826179+Clouke@users.noreply.github.com>
2023-05-03 20:45:24 +0200

    add docs

--- core/src/main/java/gg/acai/aurora/hyperparameter/TuningEvaluator.java
+++ core/src/main/java/gg/acai/aurora/hyperparameter/TuningEvaluator.java
@@ -9,8 +9,21 @@ import gg.acai.aurora.model.Trainable;
  */
 public interface TuningEvaluator {
 
+  /**
+   * Evaluates the accuracy of the trainable with the given inputs and outputs.
+   *
+   * @param trainable The trainable to evaluate.
+   * @param inputs The inputs to evaluate with.
+   * @param outputs The outputs to evaluate with.
+   * @return Returns the score metric.
+   */
   double evaluate(Trainable trainable, double[][] inputs, double[][] outputs);
 
+  /**
+   * Gets the context of this evaluator.
+   *
+   * @return Returns the context of this evaluator.
+   */
   EvaluatorScoreContext context();
 
 }
