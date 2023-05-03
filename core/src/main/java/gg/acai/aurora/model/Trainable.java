004985bbc97d680b8add0b292980f4b4aabead3c
Clouke <76826179+Clouke@users.noreply.github.com>
2023-05-03 20:45:13 +0200

    add docs

--- core/src/main/java/gg/acai/aurora/model/Trainable.java
+++ core/src/main/java/gg/acai/aurora/model/Trainable.java
@@ -3,18 +3,36 @@ package gg.acai.aurora.model;
 import gg.acai.aurora.sets.DataSet;
 
 /**
+ * A trainable model is a model that can be trained on a data set.
+ *
  * @author Clouke
  * @since 02.04.2023 22:39
  * © Aurora - All Rights Reserved
  */
 public interface Trainable extends Accuracy {
 
+  /**
+   * Trains the model on the given inputs and outputs.
+   *
+   * @param inputs The inputs to train on
+   * @param outputs The outputs to train on
+   */
   void train(double[][] inputs, double[][] outputs);
 
+  /**
+   * Trains the model on the given data set.
+   *
+   * @param set The data set to train on
+   */
   default void train(DataSet set) {
     train(set.inputs(), set.targets());
   }
 
+  /**
+   * Trains the model on pre-set data.
+   *
+   * @throws UnsupportedOperationException If the model does not support training without data.
+   */
   default void train() {
     throw new UnsupportedOperationException("This model does not support training without data.");
   }
