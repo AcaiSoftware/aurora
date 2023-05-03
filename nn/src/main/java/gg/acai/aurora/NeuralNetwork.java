12ce2d29af26d6328e3b7863da0fda417981b2c4
Clouke <76826179+Clouke@users.noreply.github.com>
2023-05-03 16:22:28 +0200

    add `wrap()`, asynchronous support, getter for `ActivationFunction`, getter for `EpochAction<...>` iterable actions

--- nn/src/main/java/gg/acai/aurora/NeuralNetwork.java
+++ nn/src/main/java/gg/acai/aurora/NeuralNetwork.java
@@ -2,11 +2,16 @@ package gg.acai.aurora;
 
 import gg.acai.acava.commons.Attributes;
 import gg.acai.acava.io.Closeable;
+import gg.acai.aurora.model.ActivationFunction;
+import gg.acai.aurora.model.EpochAction;
 import gg.acai.aurora.model.Evaluation;
 import gg.acai.aurora.model.ModelConvertible;
 import gg.acai.aurora.model.Trainable;
 import gg.acai.aurora.optimizers.Optimizer;
 import gg.acai.aurora.sets.TestSet;
+
+import java.util.Set;
+import java.util.function.Consumer;
 
 /**
  * @author Clouke
@@ -41,6 +46,13 @@ public interface NeuralNetwork extends Trainable, ModelConvertible<NeuralNetwork
   Attributes attributes();
 
   /**
+   * Gets a set of actions that are executed on each epoch iteration.
+   *
+   * @return Returns a set of actions that are executed on each epoch iteration.
+   */
+  Set<EpochAction<NeuralNetwork>> iterationActions();
+
+  /**
    * Trains this neural network with the given inputs and outputs.
    *
    * @throws IllegalStateException if no dataset was set.
@@ -73,5 +85,30 @@ public interface NeuralNetwork extends Trainable, ModelConvertible<NeuralNetwork
    */
   @Override
   NeuralNetworkModel toModel(String name);
+
+  /**
+   * Wraps this neural network into a {@link WrappedNeuralNetwork}.
+   * Can be used to get the weights and biases of this neural network.
+   *
+   * @return Returns a wrapper of this neural network.
+   */
+  WrappedNeuralNetwork wrap();
+
+  /**
+   * A completion callback that accepts a function to be executed when this
+   * neural network has completed training.
+   * <p>
+   * Sufficient for asynchronous training processes.
+   *
+   * @param function The consumer accepting this neural network.
+   */
+  void onCompletion(Consumer<NeuralNetwork> function);
+
+  /**
+   * Gets the activation function of this neural network.
+   *
+   * @return Returns the activation function of this neural network.
+   */
+  ActivationFunction activation();
 
 }
