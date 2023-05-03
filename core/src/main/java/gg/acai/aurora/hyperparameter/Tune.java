2315a7687f0b887dd219ccb21967a666078a6e56
Clouke <76826179+Clouke@users.noreply.github.com>
2023-05-03 20:45:22 +0200

    add docs

--- core/src/main/java/gg/acai/aurora/hyperparameter/Tune.java
+++ core/src/main/java/gg/acai/aurora/hyperparameter/Tune.java
@@ -7,10 +7,25 @@ package gg.acai.aurora.hyperparameter;
  */
 public interface Tune {
 
+  /**
+   * Gets the learning rate in this tune.
+   *
+   * @return Returns the learning rate in this tune.
+   */
   double learningRate();
 
+  /**
+   * Gets the epochs in this tune.
+   *
+   * @return Returns the epochs in this tune.
+   */
   int epochs();
 
+  /**
+   * Gets the layers in this tune.
+   *
+   * @return Returns the layers in this tune.
+   */
   int layers();
 
 }
