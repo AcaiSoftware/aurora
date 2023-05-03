c92a1cd3c0c6728fed58c2f3a92bd451ca888d6c
Clouke <76826179+Clouke@users.noreply.github.com>
2023-05-03 20:45:08 +0200

    add docs

--- core/src/main/java/gg/acai/aurora/publics/Ticker.java
+++ core/src/main/java/gg/acai/aurora/publics/Ticker.java
@@ -3,14 +3,26 @@ package gg.acai.aurora.publics;
 import java.util.concurrent.Callable;
 
 /**
+ * An abstract class that represents a ticker.
+ *
  * @author Clouke
  * @since 25.04.2023 17:30
  * © Aurora - All Rights Reserved
  */
 public abstract class Ticker<T> implements Callable<T> {
 
+  /**
+   * Reads the current ticker value.
+   *
+   * @return Returns the current ticker value
+   */
   public abstract T read();
 
+  /**
+   * Calls {@link #read()}.
+   *
+   * @return Returns the current ticker value
+   */
   @Override
   public T call() {
     return read();
--- core/src/main/java/gg/acai/aurora/sets/TestSet.java
+++ core/src/main/java/gg/acai/aurora/sets/TestSet.java
@@ -5,6 +5,8 @@ import java.util.Iterator;
 import java.util.Map;
 
 /**
+ * A test set for model evaluation.
+ *
  * @author Clouke
  * @since 27.04.2023 21:52
  * © Aurora - All Rights Reserved
@@ -21,22 +23,48 @@ public class TestSet implements Iterable<Map.Entry<double[], double[]>> {
     this.data = new HashMap<>();
   }
 
+  /**
+   * Adds a data point to this test set.
+   *
+   * @param input The input of the data point
+   * @param output The output of the data point
+   * @return Returns this test set for chaining
+   */
   public TestSet add(double[] input, double[] output) {
     data.put(input, output);
     return this;
   }
 
+  /**
+   * Adds a data point to this test set.
+   *
+   * @param input The input of the data point
+   * @param output The output of the data point
+   * @return Returns this test set for chaining
+   */
   public TestSet add(double[] input, double output) {
     data.put(input, new double[]{output});
     return this;
   }
 
+  /**
+   * Adds a data point to this test set.
+   *
+   * @param input The input of the data point
+   * @param output The output of the data point (true = 1.0, false = 0.0)
+   * @return Returns this test set for chaining
+   */
   public TestSet add(double[] input, boolean output) {
     double[] out = {output ? 1.0 : 0.0};
     data.put(input, out);
     return this;
   }
 
+  /**
+   * Gets this test set as a map.
+   *
+   * @return Returns this test set as a map
+   */
   public Map<double[], double[]> asMap() {
     return data;
   }
