591af0f36ca99ec4ffb2c42ea736b80b7b72e386
Clouke <76826179+Clouke@users.noreply.github.com>
2023-05-03 16:31:16 +0200

    minor tweaks

--- nn/src/main/java/gg/acai/aurora/TrainingStats.java
+++ nn/src/main/java/gg/acai/aurora/TrainingStats.java
@@ -1,9 +1,5 @@
 package gg.acai.aurora;
 
-import gg.acai.acava.commons.graph.Graph;
-
-import javax.annotation.Nullable;
-
 /**
  * @author Clouke
  * @since 03.03.2023 15:01
@@ -18,16 +14,12 @@ public class TrainingStats {
   private final double learningRate;
   private final double accuracy;
   private final long time;
-  private final double accurateEstimate;
-  private final Graph<Double> trainingGraph;
 
-  public TrainingStats(int epochs, double learningRate, double accuracy, long time, double accurateEstimate, Graph<Double> trainingGraph) {
+  public TrainingStats(int epochs, double learningRate, double accuracy, long time) {
     this.epochs = epochs;
     this.learningRate = learningRate;
     this.accuracy = accuracy;
     this.time = time;
-    this.accurateEstimate = accurateEstimate;
-    this.trainingGraph = trainingGraph;
   }
 
   public int epochs() {
@@ -46,43 +38,17 @@ public class TrainingStats {
     return time;
   }
 
-  public double accurateEstimate() {
-    return accurateEstimate;
-  }
-
-  @Nullable
-  public Graph<Double> graph() {
-    return trainingGraph;
-  }
-
   @Override
   public String toString() {
     String acc = accuracy == -1 ? "Missing Accuracy Test" : accuracy + "%";
     double seconds = time / 1000.0D;
-    int estimationDiff = (int) Math.abs(seconds - accurateEstimate);
-    String s = "\n" + ANSI_BOLD + "Training Stats:" + "\n" + RESET +
+    return "\n" + ANSI_BOLD + "Training Stats:" + "\n" + RESET +
       "• Accuracy: " + acc + "\n" +
-      "• Time: " + seconds + "s" + "\n" +
-      "• Accurate Estimate Time: " + accurateEstimate + "s" + "\n" +
-      "• Estimation Time Difference: " + estimationDiff + "s";
-
-    if (accuracy != -1 && accuracy < 80)
-      s += "\n" + ANSI_BOLD + "WARNING: Accuracy is below 80%, this may be due to a bad learning rate or a bad network architecture. (Try re-training the network)" + RESET;
-
-    return s;
+      "• Time: " + seconds + "s" + "\n";
   }
 
   public void print() {
     System.out.println(this);
-  }
-
-  public void printGraph() {
-    if (trainingGraph != null) {
-      System.out.println(trainingGraph.getVisualizer());
-      return;
-    }
-
-    System.out.println("Cannot print graph: No graph was provided.");
   }
 
 }
