55a57966a121e482c09cf5639d5fd009e9589754
Clouke <76826179+Clouke@users.noreply.github.com>
2023-05-03 16:24:21 +0200

    recode `DataSet`

--- core/src/main/java/gg/acai/aurora/sets/DataSet.java
+++ core/src/main/java/gg/acai/aurora/sets/DataSet.java
@@ -1,6 +1,7 @@
 package gg.acai.aurora.sets;
 
 import gg.acai.acava.io.Closeable;
+import gg.acai.aurora.GsonSpec;
 import gg.acai.aurora.Serializer;
 
 /**
@@ -8,23 +9,95 @@ import gg.acai.aurora.Serializer;
  * @since 28.02.2023 13:54
  * © Aurora - All Rights Reserved
  */
-public interface DataSet extends Serializer, Closeable {
+public class DataSet implements Serializer, Closeable {
 
-  static DataSetBuilder builder() {
-    return new DataSetBuilder();
+  private double[][] inputs;
+  private double[][] targets;
+
+  public static DataSet deserialize(String json) {
+    return GsonSpec.standard().fromJson(json, DataSet.class);
   }
 
-  double[][] inputs();
+  public DataSet() {
+    this.inputs = new double[0][];
+    this.targets = new double[0][];
+  }
 
-  double[][] targets();
+  public double[][] inputs() {
+    return inputs;
+  }
 
-  void add(double[][] input, double[][] target);
+  public double[][] targets() {
+    return targets;
+  }
 
-  void add(double[][] input, boolean[][] target);
+  private void resize(int newSize) {
+    double[][] newInputs = new double[newSize][];
+    double[][] newTargets = new double[newSize][];
+
+    System.arraycopy(inputs, 0, newInputs, 0, inputs.length);
+    System.arraycopy(targets, 0, newTargets, 0, targets.length);
+
+    inputs = newInputs;
+    targets = newTargets;
+  }
+
+  public void add(double[][] input, double[][] target) {
+    if (input.length != target.length)
+      throw new IllegalArgumentException("Inputs and targets must be the same size");
+
+    int size = inputs.length;
+    int newSize = size + input.length;
+
+    if (newSize > size) {
+      resize(newSize);
+    }
+
+    System.arraycopy(input, 0, inputs, size, input.length);
+    System.arraycopy(target, 0, targets, size, target.length);
+  }
+
+  public void add(double[][] input, boolean[][] target) {
+    if (input.length != target.length)
+      throw new IllegalArgumentException("Inputs and targets must be the same size");
+
+    double[][] newTarget = new double[target.length][target[0].length];
+    for (int i = 0; i < target.length; i++) {
+      for (int j = 0; j < target[i].length; j++) {
+        newTarget[i][j] = target[i][j] ? 1 : 0;
+      }
+    }
+
+    int size = inputs.length;
+    int newSize = size + input.length;
+
+    if (newSize > size) {
+      resize(newSize);
+    }
+
+    System.arraycopy(input, 0, inputs, size, input.length);
+    System.arraycopy(newTarget, 0, targets, size, newTarget.length);
+  }
 
   @Override
-  void close();
+  public String serialize() {
+    return GsonSpec.standard().toJson(this);
+  }
 
-  int size();
+  @Override
+  public void close() {
+    synchronized (this) {
+      for (int i = 0; i < inputs.length; i++) {
+        inputs[i] = null;
+        targets[i] = null;
+      }
+      inputs = null;
+      targets = null;
+    }
+  }
+
+  public int size() {
+    return inputs.length;
+  }
 
 }
