207bfc1993f73be7be8e0935506e91f5c36ab22e
Clouke <76826179+Clouke@users.noreply.github.com>
2023-05-03 16:24:38 +0200

    add default options, refactors

--- core/src/main/java/gg/acai/aurora/node/DecisionTree.java
+++ core/src/main/java/gg/acai/aurora/node/DecisionTree.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
 
 /**
  * @author Clouke
@@ -24,33 +26,20 @@ public class DecisionTree<T> {
     train(data, targetAttribute);
   }
 
-  public T predict(Map<String, T> data) {
-    Node<T> node = root;
-    while (node.attribute.hashCode() != RESULT) {
-      T value = data.get(node.attribute);
-      node = node.children.get(value);
-    }
-    return node.result;
+  public Vector<String> defaults() {
+    Vector<String> options = new Vector<>();
+    options.add("result");
+    options.add("unknown");
+    return options;
   }
 
-  private static class Node<T> {
-
-    private final String attribute;
-    private final Map<T, Node<T>> children;
-    private T result;
-
-    public Node(String attribute) {
-      this.attribute = attribute;
-      this.children = new HashMap<>();
+  public T predict(Map<String, T> data) {
+    Node<T> node = root;
+    while (node.attribute().hashCode() != RESULT) {
+      T value = data.get(node.attribute());
+      node = node.children().get(value);
     }
-
-    public void addResult(T result) {
-      this.result = result;
-    }
-
-    public void addChild(T value, Node<T> node) {
-      children.put(value, node);
-    }
+    return node.result();
   }
 
   public void train(List<Map<String, T>> data, String targetAttribute) {
@@ -68,18 +57,18 @@ public class DecisionTree<T> {
       return new Node<>("unknown");
     }
 
-    T majorityResult = getMajorityResult(data, targetAttribute);
-    if (attributes.isEmpty() || allDataHaveSameValue(data, targetAttribute)) {
+    T majorityResult = majority(data, targetAttribute);
+    if (attributes.isEmpty() || sameValues(data, targetAttribute)) {
       Node<T> leaf = new Node<>("result");
       leaf.addResult(majorityResult);
       return leaf;
     }
 
-    String bestAttribute = getBestAttribute(data, attributes, targetAttribute);
+    String bestAttribute = bestAttribute(data, attributes, targetAttribute);
 
     Node<T> node = new Node<>(bestAttribute);
-    for (T value : getDistinctValues(data, bestAttribute)) {
-      List<Map<String, T>> subData = getSubData(data, bestAttribute, value);
+    for (T value : distinctValues(data, bestAttribute)) {
+      List<Map<String, T>> subData = subData(data, bestAttribute, value);
       List<String> subAttributes = new ArrayList<>(attributes);
       subAttributes.remove(bestAttribute);
       node.addChild(value, buildTree(subData, targetAttribute, subAttributes));
@@ -93,27 +82,24 @@ public class DecisionTree<T> {
   }
 
   public T classify(Map<String, T> instance, Node<T> node) {
-    if (node.result != null) {
-      return node.result;
+    T result = node.result();
+    if (result != null) {
+      return result;
     }
 
-    T attributeValue = instance.get(node.attribute);
-    if (node.children.containsKey(attributeValue)) {
-      return classify(instance, node.children.get(attributeValue));
+    T attributeValue = instance.get(node.attribute());
+    if (node.children().containsKey(attributeValue)) {
+      return classify(instance, node.children().get(attributeValue));
     }
 
     return null;
   }
 
-  private T getMajorityResult(List<Map<String, T>> data, String targetAttribute) {
+  private T majority(List<Map<String, T>> data, String targetAttribute) {
     Map<T, Integer> countMap = new HashMap<>();
     for (Map<String, T> instance : data) {
       T result = instance.get(targetAttribute);
-      if (countMap.containsKey(result)) {
-        countMap.put(result, countMap.get(result) + 1);
-      } else {
-        countMap.put(result, 1);
-      }
+      countMap.put(result, countMap.getOrDefault(result, 0) + 1);
     }
     int maxCount = -1;
     T majorityResult = null;
@@ -127,7 +113,7 @@ public class DecisionTree<T> {
     return majorityResult;
   }
 
-  private boolean allDataHaveSameValue(List<Map<String, T>> data, String attribute) {
+  private boolean sameValues(List<Map<String, T>> data, String attribute) {
     T value = null;
     for (Map<String, T> instance : data) {
       T attributeValue = instance.get(attribute);
@@ -143,12 +129,12 @@ public class DecisionTree<T> {
   /**
    * Returns the attribute with the highest information gain.
    */
-  private String getBestAttribute(List<Map<String, T>> data, List<String> attributes, String targetAttribute) {
+  private String bestAttribute(List<Map<String, T>> data, List<String> attributes, String targetAttribute) {
     double maxGain = -1;
     String bestAttribute = null;
 
     for (String attribute : attributes) {
-      double gain = getInformationGain(data, attribute, targetAttribute);
+      double gain = infoGain(data, attribute, targetAttribute);
       if (gain > maxGain) {
         maxGain = gain;
         bestAttribute = attribute;
@@ -161,16 +147,16 @@ public class DecisionTree<T> {
   /**
    * Returns the information gain of a given attribute.
    */
-  private double getInformationGain(List<Map<String, T>> data, String attribute, String targetAttribute) {
-    double entropy = getEntropy(data, targetAttribute);
-    double remainder = getRemainder(data, attribute, targetAttribute);
+  private double infoGain(List<Map<String, T>> data, String attribute, String targetAttribute) {
+    double entropy = entropy(data, targetAttribute);
+    double remainder = remainder(data, attribute, targetAttribute);
     return entropy - remainder;
   }
 
   /**
    * Returns the entropy of the data.
    */
-  private double getEntropy(List<Map<String, T>> data, String targetAttribute) {
+  private double entropy(List<Map<String, T>> data, String targetAttribute) {
     Map<T, Integer> countMap = new HashMap<>();
     for (Map<String, T> instance : data) {
       T result = instance.get(targetAttribute);
@@ -190,11 +176,11 @@ public class DecisionTree<T> {
     return entropy;
   }
 
-  private double getRemainder(List<Map<String, T>> data, String attribute, String targetAttribute) {
+  private double remainder(List<Map<String, T>> data, String attribute, String targetAttribute) {
     double remainder = 0;
-    for (T value : getDistinctValues(data, attribute)) {
-      List<Map<String, T>> subData = getSubData(data, attribute, value);
-      remainder += ((double) subData.size() / data.size()) * getEntropy(subData, targetAttribute);
+    for (T value : distinctValues(data, attribute)) {
+      List<Map<String, T>> subData = subData(data, attribute, value);
+      remainder += ((double) subData.size() / data.size()) * entropy(subData, targetAttribute);
     }
     return remainder;
   }
@@ -202,8 +188,8 @@ public class DecisionTree<T> {
   /**
    * Returns a set of distinct values for a given attribute in the data.
    */
-  private HashSet<T> getDistinctValues(List<Map<String, T>> data, String attribute) {
-    HashSet<T> distinctValues = new HashSet<>();
+  private Set<T> distinctValues(List<Map<String, T>> data, String attribute) {
+    Set<T> distinctValues = new HashSet<>();
 
     for (Map<String, T> instance : data) {
       distinctValues.add(instance.get(attribute));
@@ -215,7 +201,7 @@ public class DecisionTree<T> {
   /**
    * Returns a subset of the data where the given attribute has the given value.
    */
-  private List<Map<String, T>> getSubData(List<Map<String, T>> data, String attribute, T value) {
+  private List<Map<String, T>> subData(List<Map<String, T>> data, String attribute, T value) {
     List<Map<String, T>> subData = new ArrayList<>();
 
     for (Map<String, T> instance : data) {
