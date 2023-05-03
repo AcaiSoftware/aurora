7c2344cd6d927c0e5b0c022a86f6e9c22f0833de
Clouke <76826179+Clouke@users.noreply.github.com>
2023-05-03 20:45:11 +0200

    add docs

--- core/src/main/java/gg/acai/aurora/TimeEstimator.java
+++ core/src/main/java/gg/acai/aurora/TimeEstimator.java
@@ -3,12 +3,17 @@ package gg.acai.aurora;
 import gg.acai.acava.collect.pairs.Pair;
 
 /**
+ * A time estimator for estimating the time remaining.
+ *
  * @author Clouke
  * @since 10.02.2023 21:20
  * © Acava - All Rights Reserved
  */
 public interface TimeEstimator<T> extends Runnable {
 
+  /**
+   * Ticks this time estimator.
+   */
   void tick();
 
   @Override
@@ -16,6 +21,12 @@ public interface TimeEstimator<T> extends Runnable {
     tick();
   }
 
+  /**
+   * Gets the estimated time remaining.
+   *
+   * @param t The object to estimate the time remaining for.
+   * @return Returns the estimated time remaining.
+   */
   double estimated(T t);
 
   Pair<Double, Time> estimateWith(T t);
