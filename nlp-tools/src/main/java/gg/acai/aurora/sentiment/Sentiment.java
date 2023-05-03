ee488bf314ba1bce1829f59de8c5521d0a3f1d65
Clouke <76826179+Clouke@users.noreply.github.com>
2023-05-03 15:15:49 +0200

    create `Sentiment` type

---
+++ nlp-tools/src/main/java/gg/acai/aurora/sentiment/Sentiment.java
@@ -0,0 +1,10 @@
+package gg.acai.aurora.sentiment;
+
+/**
+ * @author Clouke
+ * @since 03.05.2023 13:03
+ * © Aurora - All Rights Reserved
+ */
+public enum Sentiment {
+  POSITIVE, NEGATIVE, NEUTRAL
+}
