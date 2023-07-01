c53bbf3800073363c78bda2eb1410d652c86d2e9
Clouke <76826179+Clouke@users.noreply.github.com>
2023-07-01 14:36:18 +0200

    `BinaryWrapper`, wraps around path & serialization of object

---
+++ core/src/main/java/gg/acai/aurora/publics/io/bin/BinaryWrapper.java
@@ -0,0 +1,11 @@
+package gg.acai.aurora.publics.io.bin;
+
+/**
+ * @author Clouke
+ * @since 01.07.2023 14:18
+ * © Aurora - All Rights Reserved
+ */
+@FunctionalInterface
+public interface BinaryWrapper {
+  String composed();
+}
