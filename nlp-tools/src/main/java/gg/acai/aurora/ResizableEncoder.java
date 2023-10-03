529110a250267734a564bd88b709d4084cbd0718
Clouke <76826179+Clouke@users.noreply.github.com>
2023-10-03 07:59:47 +0200

    allow resizing encoders

---
+++ nlp-tools/src/main/java/gg/acai/aurora/ResizableEncoder.java
@@ -0,0 +1,11 @@
+package gg.acai.aurora;
+
+/**
+ * @author Clouke
+ * @since 17.07.2023 15:12
+ * © Aurora - All Rights Reserved
+ */
+@FunctionalInterface
+public interface ResizableEncoder {
+  void resize(int newSize);
+}
