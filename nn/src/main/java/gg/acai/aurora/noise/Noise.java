1b314282f5876925cf3495bf8210b7c660e7149a
Clouke <76826179+Clouke@users.noreply.github.com>
2023-10-03 07:56:20 +0200

    create `Noise` - noise implementations

---
+++ nn/src/main/java/gg/acai/aurora/noise/Noise.java
@@ -0,0 +1,10 @@
+package gg.acai.aurora.noise;
+
+/**
+ * @author Clouke
+ * @since 10.09.2023 04:03
+ * © Aurora - All Rights Reserved
+ */
+public interface Noise {
+  void apply(double[][] weights);
+}
